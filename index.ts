30459c1402533bfbf53aea4a261aa7bb6ad31830
Marvin Scharle <marvin.scharle@conclurer.com>
2016-10-18 12:09:21 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import moment = require('moment');
+import Moment = moment.Moment;
+
+@Pipe({name: 'dateToFormat'})
+
+export class DateToFormat implements PipeTransform {
+    public transform(value: string, targetFormat: string): string {
+        let object: Moment = moment(value);
+
+        // Return empty string if not valid
+        if (!object.isValid()) return '';
+
+        return object.format(targetFormat);
+    }
+}
---
+++ tests/main.spec.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import test = require('tape');
+import Test = test.Test;
+import {DateToFormat} from '../';
+import moment = require('moment');
+
+test('default behavior of pipe', (t: Test) => {
+    let i: DateToFormat = new DateToFormat();
+    let expected: string = moment('2016-01-09T09:00Z').format('YY-M-D H:m:s');
+
+    t.equals(i.transform('2016-01-09T09:00Z', 'YY-M-D H:m:s'), expected);
+    t.end();
+});
+
+test('invalid date behavior of pipe', (t: Test) => {
+    let i: DateToFormat = new DateToFormat();
+
+    t.equals(i.transform('Invalid Date', '2016-01-09T09:00Z'), '');
+    t.end();
+});
